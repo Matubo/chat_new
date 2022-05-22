4eeb27a7b66b4c542101bb18ebdbaf8a13f27d19
Matthew <Matbei14@yandex.ru>
2022-05-22 20:22:32 +0300

    change color 'pallete'

--- client_typescript/src/components/AppRoot/AppRoot.tsx
+++ client_typescript/src/components/AppRoot/AppRoot.tsx
@@ -3,17 +3,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store/reducers";
 import Authorization from "../Authorization/Authorization";
 import "./AppRoot.css";
-import { CREATE_ROOM_REQUEST, JOIN_TO_ROOM_REQUEST, SEND_MESSAGE, SET_USERNAME_REQUEST } from "../../queries";
+import {
+  CREATE_ROOM_REQUEST,
+  JOIN_TO_ROOM_REQUEST,
+  SEND_MESSAGE,
+  SET_USERNAME_REQUEST,
+} from "../../queries";
 import ConnectField from "../ConnectField/ConnectField";
 import ChatList from "../ChatList/ChatList";
 import { CHANGE_ROOM } from "../../store/action-creator/Actions";
 import MessageSection from "../MessageSection/MessageSection";
 
 const ChatApp: FC = () => {
-  let { authorized, username } = useSelector((store: RootState) => store.authorization);
+  let { authorized, username } = useSelector(
+    (store: RootState) => store.authorization
+  );
   let { rooms, currentRoom } = useSelector(
     (store: RootState) => store.chatRoom
   );
+  
   let dispatch = useDispatch();
 
   if (!authorized) {
@@ -26,20 +34,26 @@ const ChatApp: FC = () => {
     );
   } else {
     return (
-      <>
-{/*         <ConnectField
-          callback={(data: string) =>
-            dispatch(JOIN_TO_ROOM_REQUEST({ id: data }))
-          }
-        ></ConnectField> */}
-        <div className="chat-field-grid">
-        <ChatList rooms={rooms} currentRoom={currentRoom} createRoomFun={()=>{CREATE_ROOM_REQUEST({})}} changeRoomFun={(id:string)=>dispatch(CHANGE_ROOM({id}))} connectRoomFun={(id:string)=>JOIN_TO_ROOM_REQUEST({id})}></ChatList>
-        <MessageSection messages={currentRoom?rooms[currentRoom].messages:undefined} sendMessCallback={(message:string)=>{if(currentRoom){(SEND_MESSAGE({id:currentRoom,message}))}}} selfUsername={username}></MessageSection>
-        </div>
-        <button style={{position:"absolute",top:0,right:0}} onClick={()=>{console.log({authorized,rooms,currentRoom})}}>console.log(store)</button>
-        <button style={{position:"absolute",top:5,right:5}} onClick={()=>{JOIN_TO_ROOM_REQUEST({id:'1'})}}>join</button>
-        <button style={{position:"absolute",top:10,right:10}} onClick={()=>{CREATE_ROOM_REQUEST({})}}>create</button>
-      </>
+      <div className="chat-field-grid">
+        <ChatList
+          rooms={rooms}
+          currentRoom={currentRoom}
+          createRoomFun={() => {
+            CREATE_ROOM_REQUEST({});
+          }}
+          changeRoomFun={(id: string) => dispatch(CHANGE_ROOM({ id }))}
+          connectRoomFun={(id: string) => JOIN_TO_ROOM_REQUEST({ id })}
+        ></ChatList>
+        <MessageSection
+          messages={currentRoom ? rooms[currentRoom].messages : undefined}
+          sendMessCallback={(message: string) => {
+            if (currentRoom) {
+              SEND_MESSAGE({ id: currentRoom, message });
+            }
+          }}
+          selfUsername={username}
+        ></MessageSection>
+      </div>
     );
   }
 };
--- client_typescript/src/components/ChatList/ChatList.tsx
+++ client_typescript/src/components/ChatList/ChatList.tsx
@@ -1,50 +1,73 @@
-import React, {FC, useState} from 'react';
-import { IChatRooms } from '../../types/states/ChatRoomsTypes';
-import ChatElem from './ChatElem';
-import './ChatList.css';
+import React, { FC, useState } from "react";
+import { IChatRooms } from "../../types/states/ChatRoomsTypes";
+import ChatElem from "./ChatElem";
+import "./ChatList.css";
 
-type ChatListProps={
-    rooms:IChatRooms,
-    currentRoom:string|null,
-    createRoomFun:Function,
-    changeRoomFun:Function,
-    connectRoomFun:Function
-}
-
-const ConnectBlock = function({connect,create}:{connect:Function,create:Function}){
-const [value,setValue]=useState('0');
-
-function onChangeHandler(e:React.ChangeEvent<HTMLInputElement>){
-    setValue(e.target.value)
-}
-
-    return(  
-        <div className='connect-field'>
-           <input type="number" name="connect" onChange={onChangeHandler}/>
-           <button onClick={()=>connect(value)}>connect</button>
-           <button onClick={()=>create()}>create</button>
-       </div>
-    )
-   }
-
-const ChatList:FC<ChatListProps> = ({rooms,createRoomFun,changeRoomFun,currentRoom,connectRoomFun}) => {
-
-    let chatList = [];
-    for (let roomId in rooms) {
-        const {id,name,base64Image}=rooms[roomId]
-        chatList.push(<ChatElem id={id} name={name} base64Image={base64Image} self={roomId==currentRoom?true:false} callback={()=>{changeRoomFun(id)}} key={id}></ChatElem>)
-      }
-
-    return (
-        <div className='left-block'>
-            <ConnectBlock connect={connectRoomFun} create={createRoomFun}></ConnectBlock>
-        <div className='chat-list'>   
-            {chatList}
-        </div>
-        </div>
-    );
+type ChatListProps = {
+  rooms: IChatRooms;
+  currentRoom: string | null;
+  createRoomFun: Function;
+  changeRoomFun: Function;
+  connectRoomFun: Function;
 };
 
+const ConnectBlock = function ({
+  connect,
+  create,
+}: {
+  connect: Function;
+  create: Function;
+}) {
+  const [value, setValue] = useState("0");
 
+  function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+    setValue(e.target.value);
+  }
 
-export default ChatList
+  return (
+    <div className="connect-field">
+      <input type="number" name="connect" onChange={onChangeHandler} />
+      <button onClick={() => connect(value)}>connect</button>
+      <button onClick={() => create()}>create</button>
+    </div>
+  );
+};
+
+const ChatList: FC<ChatListProps> = ({
+  rooms,
+  createRoomFun,
+  changeRoomFun,
+  currentRoom,
+  connectRoomFun,
+}) => {
+  let chatList = [];
+  for (let roomId in rooms) {
+    const { id, name, base64Image } = rooms[roomId];
+    chatList.push(
+      <ChatElem
+        id={id}
+        name={name}
+        base64Image={base64Image}
+        self={roomId == currentRoom ? true : false}
+        callback={() => {
+          changeRoomFun(id);
+        }}
+        key={id}
+      ></ChatElem>
+    );
+  }
+
+  return (
+    <div className="left-block">
+      <div className="chat-list">
+        <ConnectBlock
+          connect={connectRoomFun}
+          create={createRoomFun}
+        ></ConnectBlock>
+        {chatList}
+      </div>
+    </div>
+  );
+};
+
+export default ChatList;
