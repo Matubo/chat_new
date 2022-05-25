5fb2172d020bffb472195c2affda84db15c659b7
MattieChu <MattieChu@users.noreply.github.com>
2022-05-25 17:22:37 +0300

    add message 'tail'

--- client_typescript/src/components/ConnectField/ConnectField.tsx
+++ client_typescript/src/components/ConnectField/ConnectField.tsx
@@ -1,23 +1,32 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState } from 'react';
 
 type ConnectFieldPropTypes = {
-  callback: Function;
+	callback: Function;
 };
 
 const ConnectField: FC<ConnectFieldPropTypes> = ({ callback }) => {
-  const [room, setRoom] = useState("");
-  const changeInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setRoom(e.target.value);
-  };
-  const clickHandler = () => {
-    callback(room);
-  };
-  return (
-    <div className="connect-field">
-      <input className="connect-field__input input" type="number" name="" value={room} onChange={changeInputHandler} />
-      <button className="connect-field__button button" onClick={clickHandler}></button>
-    </div>
-  );
+	const [room, setRoom] = useState('');
+	const changeInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setRoom(e.target.value);
+	};
+	const clickHandler = () => {
+		callback(room);
+	};
+	return (
+		<div className="connect-field">
+			<input
+				className="connect-field__input input"
+				type="number"
+				name=""
+				value={room}
+				onChange={changeInputHandler}
+			/>
+			<button
+				className="connect-field__button button"
+				onClick={clickHandler}
+			></button>
+		</div>
+	);
 };
 
 export default ConnectField;
