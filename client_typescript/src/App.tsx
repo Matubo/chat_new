c1318fa6750a899dcb4a8f7bc675f01f57d42ef6
MattieChu <MattieChu@users.noreply.github.com>
2022-05-30 09:38:46 +0300

    clean garbage

--- client_typescript/src/App.tsx
+++ client_typescript/src/App.tsx
@@ -1,16 +1,15 @@
-import "./App.css";
-import { store } from "./store/store";
-import {Provider } from "react-redux";
-import AppRoot from "./components/AppRoot/AppRoot";
-import logo from './assets/img/bubble-chat.png';
-import './assets/fonts/stylesheet.css'
+import './App.css';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
+import AppRoot from './components/AppRoot/AppRoot';
+import './assets/fonts/stylesheet.css';
 
-export default function App()  {
-  return (
-    <div className="App">
-        <Provider store={store}>
-         <AppRoot></AppRoot>
-      </Provider>
-    </div>
-  );
+export default function App() {
+	return (
+		<div className="App">
+			<Provider store={store}>
+				<AppRoot></AppRoot>
+			</Provider>
+		</div>
+	);
 }
