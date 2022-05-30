36941dd960f472480f670d8f00b9c4f69a55795b
MattieChu <MattieChu@users.noreply.github.com>
2022-05-30 10:16:30 +0300

    prettier

--- client_typescript/src/store/reducers/Authorization.ts
+++ client_typescript/src/store/reducers/Authorization.ts
@@ -1,31 +1,31 @@
 import {
-  AuthorizationActionTypes,
-  AuthorizationAction,
-} from "../../types/actions/AuthorizationActions";
-import { IAuthorization } from "../../types/states/AuthorizationTypes";
+	AuthorizationActionTypes,
+	AuthorizationAction,
+} from '../../types/actions/AuthorizationActions';
+import { IAuthorization } from '../../types/states/AuthorizationTypes';
 
 const initialState: IAuthorization = {
-  authorized: false,
-  username: null,
+	authorized: false,
+	username: null,
 };
 
 export const authorizationReducer = (
-  state = initialState,
-  action: AuthorizationAction
+	state = initialState,
+	action: AuthorizationAction
 ): IAuthorization => {
-  switch (action.type) {
-    case AuthorizationActionTypes.LOG_IN:
-      const { username }=action.payload;
-      return {
-        authorized: true,
-        username
-      };
-    case AuthorizationActionTypes.LOG_OUT:
-      return {
-        authorized: false,
-        username: null,
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case AuthorizationActionTypes.LOG_IN:
+			const { username } = action.payload;
+			return {
+				authorized: true,
+				username,
+			};
+		case AuthorizationActionTypes.LOG_OUT:
+			return {
+				authorized: false,
+				username: null,
+			};
+		default:
+			return state;
+	}
 };
--- client_typescript/src/store/reducers/ChatRooms.ts
+++ client_typescript/src/store/reducers/ChatRooms.ts
@@ -1,34 +1,34 @@
 import {
-  ChatRoomsActions,
-  ChatRoomsActionTypes,
-} from "../../types/actions/ChatRoomsActions";
-import {  IChatRoomsState } from "../../types/states/ChatRoomsTypes";
+	ChatRoomsActions,
+	ChatRoomsActionTypes,
+} from '../../types/actions/ChatRoomsActions';
+import { IChatRoomsState } from '../../types/states/ChatRoomsTypes';
 
 const initialState: IChatRoomsState = {
-  rooms: {},
-  currentRoom: null,
+	rooms: {},
+	currentRoom: null,
 };
 
 export const chatRoomsReducer = (
-  state = initialState,
-  action: ChatRoomsActions
+	state = initialState,
+	action: ChatRoomsActions
 ): IChatRoomsState => {
-  switch (action.type) {
-    case ChatRoomsActionTypes.ADD_ROOM: {
-      const { room } = action.payload;
-      state.rooms[room.id] = room;
-      return { ...state };
-    }
-    case ChatRoomsActionTypes.CHANGE_ROOM: {
-      const { id } = action.payload;
-      return { ...state, currentRoom: id };
-    }
-    case ChatRoomsActionTypes.SET_NEW_MESSAGE: {
-      const { id, message } = action.payload;
-      state.rooms[id].messages.push(message);
-      return { ...state };
-    }
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case ChatRoomsActionTypes.ADD_ROOM: {
+			const { room } = action.payload;
+			state.rooms[room.id] = room;
+			return { ...state };
+		}
+		case ChatRoomsActionTypes.CHANGE_ROOM: {
+			const { id } = action.payload;
+			return { ...state, currentRoom: id };
+		}
+		case ChatRoomsActionTypes.SET_NEW_MESSAGE: {
+			const { id, message } = action.payload;
+			state.rooms[id].messages.push(message);
+			return { ...state };
+		}
+		default:
+			return state;
+	}
 };
