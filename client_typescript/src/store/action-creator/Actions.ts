36b4033e01cc0ad1d577c5a724338552aa365111
MattieChu <MattieChu@users.noreply.github.com>
2022-05-30 11:25:47 +0300

    del conlog

--- client_typescript/src/store/action-creator/Actions.ts
+++ client_typescript/src/store/action-creator/Actions.ts
@@ -1,51 +1,47 @@
-import { Dispatch } from "react";
+import { Dispatch } from 'react';
 import {
-  ChatRoomsActions,
-  ChatRoomsActionTypes,
-} from "../../types/actions/ChatRoomsActions";
+	ChatRoomsActions,
+	ChatRoomsActionTypes,
+} from '../../types/actions/ChatRoomsActions';
 import {
-  AuthorizationAction,
-  AuthorizationActionTypes,
-} from "../../types/actions/AuthorizationActions";
+	AuthorizationAction,
+	AuthorizationActionTypes,
+} from '../../types/actions/AuthorizationActions';
 import {
-  AcceptSetUsernameFunctionParams,
-  AddRoomFunctionParams,
-  ChangeRoomFunctionParams,
-  NewRoomMessageFunctionParams,
-} from "../../types/queries/responses";
+	AcceptSetUsernameFunctionParams,
+	AddRoomFunctionParams,
+	ChangeRoomFunctionParams,
+	NewRoomMessageFunctionParams,
+} from '../../types/queries/responses';
 
 export const SET_NEW_MESSAGES = (
-  data: NewRoomMessageFunctionParams
+	data: NewRoomMessageFunctionParams
 ): ChatRoomsActions => {
-  console.log("SET_NEW_MESSAGES", data);
-  const { id, message } = data;
-  return {
-    type: ChatRoomsActionTypes.SET_NEW_MESSAGE,
-    payload: { id: id, message: message },
-  };
+	const { id, message } = data;
+	return {
+		type: ChatRoomsActionTypes.SET_NEW_MESSAGE,
+		payload: { id: id, message: message },
+	};
 };
 
 export const CHANGE_ROOM = (data: ChangeRoomFunctionParams) => {
-  return (dispatch: Dispatch<ChatRoomsActions>) => {
-    console.log("CHANGE_ROOM", data);
-    const { id } = data;
-    dispatch({ type: ChatRoomsActionTypes.CHANGE_ROOM, payload: { id } });
-  };
+	return (dispatch: Dispatch<ChatRoomsActions>) => {
+		const { id } = data;
+		dispatch({ type: ChatRoomsActionTypes.CHANGE_ROOM, payload: { id } });
+	};
 };
 
 export const SET_USERNAME = (
-  data: AcceptSetUsernameFunctionParams
+	data: AcceptSetUsernameFunctionParams
 ): AuthorizationAction => {
-  console.log("SET_USERNAME", data);
-  const { username } = data;
-  return { type: AuthorizationActionTypes.LOG_IN, payload: { username } };
+	const { username } = data;
+	return { type: AuthorizationActionTypes.LOG_IN, payload: { username } };
 };
 
 export const ADD_ROOM = (data: AddRoomFunctionParams): ChatRoomsActions => {
-  console.log("ADD_ROOM", data);
-  const { room } = data;
-  return {
-    type: ChatRoomsActionTypes.ADD_ROOM,
-    payload: { room },
-  };
+	const { room } = data;
+	return {
+		type: ChatRoomsActionTypes.ADD_ROOM,
+		payload: { room },
+	};
 };
